0373df1168ade5d37a41f535eb6c0cf94df5d547
Jasper Roeken <39262694+Jasphur@users.noreply.github.com>
2019-01-03 12:39:22 +0100

    Add files via upload

---
+++ TextAnalyzerConsoleVersion/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace TextAnalyzerConsoleVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader("TestFile.txt"))
+                {
+                    int charCount = 0;
+                    int whiteSpaceCount = 0;
+                    int lineCount = 0;
+                    int wordCount = 0;
+                    string Separators = ",. ?!";
+                    string line;
+
+                    // Reading the file
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        lineCount++;
+
+                        foreach (var c in line)
+                        {
+                            // Character count
+                            charCount++;
+
+                            // Whitespace count, so character count without spaces can be calculated.
+                            if (Char.IsWhiteSpace(c))
+                                whiteSpaceCount++;
+
+                            // Word counter
+                            if (Separators.Contains(c))
+                            {
+                                wordCount++;
+                            }
+                        }
+
+                        Console.WriteLine(line);
+                    }
+
+                    Console.WriteLine("");
+                    Console.WriteLine("The number of characters used: " + charCount);
+                    Console.WriteLine("The number of characters used without space: " + (charCount - whiteSpaceCount));
+                    Console.WriteLine("Number of words: " + wordCount);
+                    Console.WriteLine("Number of lines: " + lineCount);
+                }
+            }
+            catch (Exception e)
+            { 
+                // Error message when file cannot be read
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press <Enter> to exit..");
+            Console.ReadLine();
+        }
+    }
+}
